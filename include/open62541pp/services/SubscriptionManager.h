8151928aae9f6412081704944ce41076c75704e1
Eduardo Lopes <elopes1@oceaneering.com>
2023-04-13 01:53:46 -0300

    MVP of the subscription manager is complete, but lacking any tests at this point
    Signed-off-by: Eduardo Lopes <elopes1@oceaneering.com>

--- include/open62541pp/services/SubscriptionManager.h
+++ include/open62541pp/services/SubscriptionManager.h
@@ -3,28 +3,34 @@
 #include "open62541pp/Client.h"
 #include "open62541pp/types/AttributeId.h"
 #include "open62541pp/types/MonitoredItem.h"
+#include "open62541pp/types/MonitoringMode.h"
 #include "open62541pp/types/NodeId.h"
 #include "open62541pp/types/Subscription.h"
 
 #include <chrono>
 #include <memory>
+#include <optional>
 
 namespace opcua {
 namespace services {
 
-    /**
+/**
  * @brief Create a subscription to a client
  * @param client opcua client
  * @param period period of the update
  * @return shared_ptr to Subscription
+ * @todo add the possibility of customize the request
+ * @throws std::runtime_error if the client is unavailable
  */
-std::shared_ptr<Subscription> createSubscription(std::weak_ptr<Client> client, std::chrono::milliseconds period
+std::shared_ptr<Subscription> createSubscription(
+    std::weak_ptr<Client> client, std::chrono::milliseconds period
 );
 
 /**
  * @brief Remove the subscription from the client
  * @param client opcua client
  * @param subscription subscription to remove
+ * @throws std::runtime_error if the client is unavailable
  */
 void removeSubscription(std::weak_ptr<Client> client, std::weak_ptr<Subscription> subscription);
 
@@ -33,24 +39,44 @@ void removeSubscription(std::weak_ptr<Client> client, std::weak_ptr<Subscription
  * @param client opcua client
  * @param subscription subscription to update
  * @param attributeId attribute id
+ * @param mode monitoring mode
  * @param callback from the user
+ * @throws std::runtime_error if the client or the subscription is unavailable
+ * @todo add the possibility of customize the request
  */
 std::shared_ptr<MonitoredItem> monitorAttributeFromNode(
-    std::weak_ptr<Subscription> subscription, const NodeId& nodeId, AttributeId attributeId,
-    AttributeChangedCallback callback
+    std::weak_ptr<Client> client,
+    std::weak_ptr<Subscription> subscription,
+    const NodeId& nodeId,
+    AttributeId attributeId,
+    MonitoringMode mode,
+    std::optional<AttributeChangedCallback> callback
 );
 
 /**
  * @register registers a callback of data changes from a NodeId
+ * @param client opcua client
  * @param subscription
  * @param nodeId
- * @param callback
- * @return
+ * @param callback user callback
+ * @return shared_ptr to MonitoredItem
+ * @throws std::runtime_error if the client or the subscription is unavailable
+ * @todo add the possibility of customize the request
  */
 std::shared_ptr<MonitoredItem> monitorValueChangesFromNode(
-    std::weak_ptr<Subscription> subscription, const NodeId& nodeId,
-    DataChangedCallback callback
+    std::weak_ptr<Client> client,
+    std::weak_ptr<Subscription> subscription,
+    const NodeId& nodeId,
+    std::optional<DataChangedCallback> callback
 );
+
+/**
+ * @brief run the iterate method from the UA_client
+ * @param client
+ * @param timeout
+ */
+
+void iterateSubscription(std::shared_ptr<Client> client, uint32_t timeout = 1000);
 
 }  // namespace services
 }  // namespace opcua
--- src/services/SubscriptionManager.cpp
+++ src/services/SubscriptionManager.cpp
@@ -4,25 +4,180 @@
 
 #include "open62541pp/services/SubscriptionManager.h"
 
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/client_subscriptions.h>
+
+#include <sstream>
+
+namespace {
+auto constexpr LAST_CHECK_TIMEOUT = 100;
+}
+
 namespace opcua {
 namespace services {
 
-std::shared_ptr<Subscription> createSubscription(std::weak_ptr<Client> client, std::chrono::milliseconds period
-) {
-    return {};
+inline std::shared_ptr<Client> lockOrThrowClient(std::weak_ptr<Client> client) {
+    auto sharedClient = client.lock();
+
+    if (!sharedClient) {
+        throw std::runtime_error("Client has been destroyed");
+    }
+    return sharedClient;
 }
-void removeSubscription(std::weak_ptr<Client> client, std::shared_ptr<Subscription> subscription) {}
+
+inline std::shared_ptr<Subscription> lockOrThrowSubscription(
+    std::weak_ptr<Subscription> subscription
+) {
+    auto sharedSubscription = subscription.lock();
+
+    if (!sharedSubscription) {
+        throw std::runtime_error("Subscription has been destroyed");
+    }
+    return sharedSubscription;
+}
+
+inline std::shared_ptr<MonitoredItem> lockOrThrowMonitoredItems(std::weak_ptr<MonitoredItem> item) {
+    auto sharedItem = item.lock();
+
+    if (!sharedItem) {
+        throw std::runtime_error("MonitoredItem has been destroyed");
+    }
+    return sharedItem;
+}
+
+static void monitoredValueChangedCallback(
+    UA_Client* client,
+    UA_UInt32 subId,
+    void* subContext,
+    UA_UInt32 monId,
+    void* monContext,
+    UA_DataValue* value
+) {
+    (void)client;
+    (void)subId;
+    (void)subContext;
+
+    auto monitoredItem = static_cast<MonitoredItem*>(monContext);
+
+    if (monitoredItem) {
+        // saving the values just in case the callback is not available
+        monitoredItem->lastReading = DataValue(*value);
+        monitoredItem->monitorId = monId;
+
+        // run the callback
+        if (monitoredItem->dataChangedCallback) {
+            std::invoke(monitoredItem->dataChangedCallback.value(), monId, DataValue(*value));
+        }
+    }
+}
+
+std::shared_ptr<Subscription> createSubscription(
+    std::weak_ptr<Client> client, std::chrono::milliseconds period
+) {
+    auto sharedClient = lockOrThrowClient(client);
+
+    auto subscription = std::make_shared<Subscription>();
+
+    UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
+    request.requestedPublishingInterval = period.count();
+
+    UA_CreateSubscriptionResponse response = UA_Client_Subscriptions_create(
+        sharedClient->handle(), request, subscription.get(), NULL, NULL
+    );
+
+    subscription->subscriptionId = response.subscriptionId;
+    subscription->revisedPublishingInterval = static_cast<double>(response.revisedPublishingInterval
+    );
+    subscription->revisedLifetimeCount = static_cast<double>(response.revisedLifetimeCount);
+    subscription->revisedPublishingInterval = static_cast<double>(response.revisedPublishingInterval
+    );
+
+    return subscription;
+}
+
+void removeSubscription(std::weak_ptr<Client> client, std::shared_ptr<Subscription> subscription) {
+    auto sharedClient = lockOrThrowClient(client);
+
+    UA_Client_run_iterate(sharedClient->handle(), LAST_CHECK_TIMEOUT);
+    /* Delete the subscription */
+    if (UA_Client_Subscriptions_deleteSingle(
+            sharedClient->handle(), subscription->subscriptionId
+        ) != UA_STATUSCODE_GOOD) {
+        throw std::runtime_error("Failed to delete subscription");
+    }
+}
 
 std::shared_ptr<MonitoredItem> monitorAttributeFromNode(
-    std::weak_ptr<Subscription> subscription, const NodeId& nodeId, AttributeId attributeId
-){
-    return {};
+    std::weak_ptr<Client> client,
+    std::weak_ptr<Subscription> subscription,
+    const NodeId& nodeId,
+    AttributeId attributeId,
+    MonitoringMode mode,
+    std::optional<AttributeChangedCallback> callback
+) {
+    auto sharedClient = lockOrThrowClient(client);
+
+    auto sharedSubscription = lockOrThrowSubscription(subscription);
+
+    auto monitoredItem = std::make_shared<MonitoredItem>();
+
+    if (callback) {
+        monitoredItem->attributeChangedCallback = callback;
+    }
+
+    UA_MonitoredItemCreateRequest item;
+    UA_MonitoredItemCreateRequest_init(&item);
+    item.itemToMonitor.nodeId = *(nodeId.handle());
+    item.itemToMonitor.attributeId = static_cast<UA_UInt32>(attributeId);
+    item.monitoringMode = static_cast<UA_MonitoringMode>(mode);
+
+    // hint
+    // https://github.com/qt/qtopcua/blob/5.15.2/src/plugins/opcua/open62541/qopen62541subscription.cpp
+    throw std::runtime_error("Not implemented yet");
 }
 
 std::shared_ptr<MonitoredItem> monitorValueChangesFromNode(
-    std::weak_ptr<Subscription> subscription, const NodeId& nodeId
-){
-    return {};
+    std::weak_ptr<Client> client,
+    std::weak_ptr<Subscription> subscription,
+    const NodeId& nodeId,
+    std::optional<DataChangedCallback> callback
+) {
+    auto sharedClient = lockOrThrowClient(client);
+
+    auto sharedSubscription = lockOrThrowSubscription(subscription);
+
+    auto monitoredItem = std::make_shared<MonitoredItem>();
+
+    UA_MonitoredItemCreateRequest monRequest = UA_MonitoredItemCreateRequest_default(
+        *(nodeId.handle())
+    );
+
+    UA_MonitoredItemCreateResult monResponse = UA_Client_MonitoredItems_createDataChange(
+        sharedClient->handle(),
+        sharedSubscription->subscriptionId,
+        UA_TIMESTAMPSTORETURN_BOTH,
+        monRequest,
+        monitoredItem.get(),
+        monitoredValueChangedCallback,
+        NULL
+    );
+    if (monResponse.statusCode != UA_STATUSCODE_GOOD) {
+        std::stringstream ss;
+        ss << "Failed to create monitored item for node: "
+           << nodeId.getIdentifierAs<opcua::String>().get();
+        throw std::runtime_error(ss.str());
+    }
+    if (callback) {
+        monitoredItem->dataChangedCallback = callback;
+    }
+
+    return monitoredItem;
+}
+
+void iterateSubscription(std::shared_ptr<Client> client, uint32_t timeout) {
+    /* The first publish request should return the initial value of the variable */
+    UA_Client_run_iterate(client->handle(), timeout);
 }
 
 }  // namespace services
